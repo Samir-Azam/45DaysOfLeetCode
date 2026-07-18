fb20d290512a1268f44b31f2ed0899b0b8233d99
Samir Azam <azamsamir190@gmail.com>
2026-07-18 23:54:30 +0530

    Time: 13 ms (14.05%), Space: 13.4 MB (37.35%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {
+
+
         unordered_map<char, int> mp = {
         {'I', 1},
         {'V', 5},
@@ -11,60 +13,15 @@ public:
         {'M', 1000}
         };
 
-        int ans = 0;
-        int i = 0;
         int n = s.size();
-        while (i<s.size()){
-            char curr = s[i];
-
-            if (curr=='I'){
-                if (i+1<n && s[i+1]=='V'){
-                    ans += 4;
-                    i = i+2;
-                }
-                else if (i+1<n && s[i+1]=='X') {
-                    ans += 9;
-                    i = i+2;
-                }
-                else {
-                    ans+= 1;
-                    i++;
-                }
-            } 
-            else if (curr=='X'){
-                if (i+1<n && s[i+1]=='L'){
-                    ans += 40;
-                    i = i+2;
-                }
-                else if (i+1<n && s[i+1]=='C') {
-                    ans += 90;
-                    i = i+2;
-                }
-                else {
-                    ans+= 10;
-                    i++;
-                }
-            }
-
-            else if(curr=='C'){
-                if (i+1<n && s[i+1]=='D'){
-                    ans += 400;
-                    i = i+2;
-                }
-                else if (i+1<n && s[i+1]=='M') {
-                    ans += 900;
-                    i = i+2;
-                }
-                else {
-                    ans+= 100;
-                    i++;
-                }
-            }
-            else {
-                ans += mp[curr];
-                i++;
-            }
+        int ans  = 0;
+        for (int i=0;i<n-1;i++){
+            int curr = mp[s[i]];
+            int next = mp[s[i+1]];
+            if (curr<next) ans-=curr;
+            else ans+=curr;
         }
+        ans+=mp[s[n-1]];
         return ans;
     }
 };
