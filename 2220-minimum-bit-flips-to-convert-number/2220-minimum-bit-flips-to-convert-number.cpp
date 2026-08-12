ef345c8b1ca3efc88401d28e79639a3ac7a6b395
Samir Azam <azamsamir190@gmail.com>
2026-08-12 16:08:44 +0530

    Time: 0 ms (100%), Space: 8 MB (60.83%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -4,7 +4,7 @@ public:
         // It is very simple we just have to figure out how many bits are different in both of them (start and goal).
         int count = 0;
         for (int i=0;i<32;i++){
-            if ((bool(start & (1<<i))) != (bool(goal & (1<<i)))) count++;
+            if ((start & (1<<i)) != (goal & (1<<i))) count++;
         }
         return count;
     }
