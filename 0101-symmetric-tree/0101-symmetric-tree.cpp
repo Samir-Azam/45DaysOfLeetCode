c1f04559f9eac6bab9d317e97683c371be0b4d1d
Samir Azam <azamsamir190@gmail.com>
2026-09-14 14:05:21 +0530

    Time: 0 ms (100%), Space: 18.6 MB (42.26%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -14,10 +14,7 @@ public:
     bool helper(TreeNode* p, TreeNode* q){
         if (!p || !q) return p==q;
 
-        if (p->val!=q->val) return false;
-        if (helper(p->left, q->right)==false) return false;
-        if (helper(p->right, q->left)==false) return false;
-        return true;
+        return (p->val==q->val && helper(p->left, q->right) && helper(p->right, q->left));
     }
     bool isSymmetric(TreeNode* root) {
         return helper(root->left, root->right);
